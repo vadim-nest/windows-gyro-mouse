be941889a64296e272645c7332442d0181f66204
Vadim G. <vadim@gne.me.uk>
2026-04-17 11:59:29 +0100

    feat(win): add SendInput P/Invoke and test button

--- GyroMouse/GyroMouse/MainWindow.xaml.cs
+++ GyroMouse/GyroMouse/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using System.Runtime.InteropServices;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -36,6 +37,43 @@ namespace GyroMouse
             {
                 LogBox.Text += $"[{DateTime.Now:HH:mm:ss.fff}] {message}\n";
             });
+        }
+
+        [DllImport("user32.dll")]
+        private static extern uint SendInput(uint nInputs, INPUT[] pInputs, int cbSize);
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct INPUT
+        {
+            public uint type;
+            public MOUSEINPUT mi;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct MOUSEINPUT
+        {
+            public int dx, dy;
+            public uint mouseData, dwFlags, time;
+            public nint dwExtraInfo;
+        }
+
+        private const uint INPUT_MOUSE = 0;
+        private const uint MOUSEEVENTF_MOVE = 0x0001;
+
+        private void MoveMouse(int dx, int dy)
+        {
+            var input = new INPUT
+            {
+                type = INPUT_MOUSE,
+                mi = new MOUSEINPUT { dx = dx, dy = dy, dwFlags = MOUSEEVENTF_MOVE }
+            };
+            SendInput(1, [input], Marshal.SizeOf<INPUT>());
+        }
+
+        private void TestMouseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MoveMouse(50, 50);
+            Log("Mouse nudged +50, +50");
         }
     }
 }
