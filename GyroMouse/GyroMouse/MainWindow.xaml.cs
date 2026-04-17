996163ed3c1a8710cb379028b4dafd0e69c16c7b
Vadim G. <vadim@gne.me.uk>
2026-04-17 11:55:54 +0100

    feat(win): add scrolling log UI

---
+++ GyroMouse/GyroMouse/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace GyroMouse
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Log("App started.");
+        }
+
+        private void Log(string message)
+        {
+            // Must run on the UI thread
+            DispatcherQueue.TryEnqueue(() =>
+            {
+                LogBox.Text += $"[{DateTime.Now:HH:mm:ss.fff}] {message}\n";
+            });
+        }
+    }
+}
